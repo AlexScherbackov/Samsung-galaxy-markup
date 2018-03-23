e56605111069c8d67ebe36252b0f9ce297606524
Alex <alexandr.shch@modesco.ru>
2018-03-23 15:36:28 +0300

    index page desctop and mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,21 @@ svg4everybody();
 
 // @include('detect.js')
 // @include('globals.js')
+
+
+const _init = {
+	initialization(){
+		_init._eventBinding();
+	},
+	_eventBinding(){
+		const mobileBtn = document.querySelector('#mobile-btn');
+		mobileBtn.addEventListener('click', ()=>{
+			const fixedMenu = document.querySelector('#mobile-menu');
+			fixedMenu.classList.toggle('open');
+			mobileBtn.classList.toggle('close');
+		});
+	}
+}
+
+
+$(document).ready(_init.initialization);
